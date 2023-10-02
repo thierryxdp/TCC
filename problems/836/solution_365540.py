81c3a5fa99113eaac803cefa89046360dfc5b93c
Thierry Dutoit <thierrydutoit123@gmail.com>
2023-10-01 23:45:48 -0300

    cria processo semi automatico para criacao de testes unitarios

--- problems/836/solution_365540.py
+++ problems/836/solution_365540.py
@@ -1,8 +1,12 @@
 def busca(setor, matriz):
     ''' dada a matriz de 4 colunas com as informacoes dos funcionarios de uma empresa na ordem: nome, registro, setor e telefone e o setor desejado, retorna as os dados dos funcionarios daquele setor
     matriz, str -> matriz'''
-    resultbusca = []
-    for i in range(len(matriz)):
-        if matriz[i][2] == setor:
-            resultbusca = resultbusca + matriz[i][0:2]+[matriz[i][3]]
-    return resultbusca
+    result = []
+    for line in matriz:
+        if (line[2] == setor):
+            result.append(line[0:2] + [line[3]])
+    
+    if(len(result) == 0):
+        return "Nenhum registro encontrado"
+    
+    return result
--- solucao.py
+++ solucao.py
@@ -30,19 +30,28 @@ def concatenacao4_com_tipo(a: str, b: str) -> str:
 def busca(setor,matriz):
     '''funcao que dado o nome de um setor da empresa, retorna os dados de todos os funcionarios daquele setor
     matriz->lista'''
-    listaresultados=[]
-    for i in range(len(matriz)):
-                   for j in range(len(matriz[i])):
-                        if matriz[i][2]==setor:
-                            listaresultados.append(matriz[i])
-    return listaresultados
+    result = []
+    for line in matriz:
+        if (line[2] == setor):
+            result.append(line[0:2] + [line[3]])
+    
+    if(len(result) == 0):
+        return "Nenhum registro encontrado"
+    
+    return result
 
 def busca_com_tipo(setor: str, matriz: list[list]) -> list[list]:
     '''funcao que dado o nome de um setor da empresa, retorna os dados de todos os funcionarios daquele setor
     matriz->lista'''
-    listaresultados=[]
-    for i in range(len(matriz)):
-                   for j in range(len(matriz[i])):
-                        if matriz[i][2]==setor:
-                            listaresultados.append(matriz[i])
-    return listaresultados
+    result = []
+    for line in matriz:
+        if (line[2] == setor):
+            result.append(line[0:2] + [line[3]])
+    
+    if(len(result) == 0):
+        return "Nenhum registro encontrado"
+    
+    return result
+
+def diff_datas_com_tipo(data1: str, data2:str) -> int:
+    return 365*(int(data2[6:10]) - int(data1[6:10])) + 30*(int(data2[3:5]) - int(data1[3:5])) + int(data2[0:2]) - int(data1[0:2])
