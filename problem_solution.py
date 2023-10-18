455fba358fd227d9921b16662af603d3bff83159
Thierry Dutoit <thierrydutoit123@gmail.com>
2023-10-18 00:29:33 -0300

    teste

--- problem_solution.py
+++ problem_solution.py
@@ -1,15 +1,5 @@
-# def intercala(lista1, lista2):
-#     lista3 = 6 * [0]
-#     lista3[::2] = lista1
-#     lista3[1::2] = lista2
-#     return lista3
-
-
 def intercala(lista1, lista2):
-    for e1 in lista1:
-        print("a")
-        lista2.append(e1)
-    for e2 in lista2:
-        print("b")
-        lista1.append(e2)
-    return lista1
+    lista3 = 6 * [0]
+    lista3[::2] = lista1
+    lista3[1::2] = lista2
+    return lista3
--- tests/tests1.py
+++ tests/tests1.py
@@ -2,4 +2,8 @@ from problem_solution import intercala
 
 
 def test_intercala():
-    assert intercala([1, 3, 5], [2, 4, 6]) == [1, 3, 5, 2, 4, 6, 1, 3, 5]
+    assert intercala([1, 3, 5], [2, 4, 6]) == [1, 2, 3, 4, 5, 6]
+
+
+def test_intercala_2():
+    assert intercala([7, 9, 11], [8, 10, 12]) == [7, 8, 9, 10, 11, 12]
