ac1700390e3bd09bce4b6eb33647dd0aca28f184
Thierry Dutoit <thierrydutoit123@gmail.com>
2023-10-17 23:41:21 -0300

    commit para tentar usar o coveralls

--- tests/tests1.py
+++ tests/tests1.py
@@ -2,4 +2,4 @@ from problem_solution import intercala
 
 
 def test_intercala():
-    assert intercala([1, 3, 5], [2, 4, 6]) == [1, 2, 3, 4, 5, 6]
+    assert intercala([1, 3, 5], [2, 4, 6]) == [1, 3, 5, 2, 4, 6, 1, 3, 5]
